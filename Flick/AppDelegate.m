72f4f0fbe763021dcaed4c67589fc2d7a3233a85
nirbhayg <nirbhay.agarwal@gmail.com>
2014-10-07 17:12:41 +0530

    Initial Commit

--- Flick/AppDelegate.m
+++ Flick/AppDelegate.m
@@ -11,16 +11,78 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
+@property (strong, nonatomic) NSStatusItem *statusItem;
+@property (assign, nonatomic) BOOL darkModeOn;
+
+//@property (strong, nonatomic) NSPipe * outputPipe;
+@property (strong, nonatomic) NSTask * buildTask;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [_statusItem setToolTip:@"Flick to Dark Mode, thanks to coolguy87"];
+ 
+    [_statusItem setAction:@selector(toggled:)];
+    [_statusItem setDoubleAction:@selector(quitApp:)];
+    
+    [self updateDarkMode];
+    [self setImage];
+}
+
+- (void)updateDarkMode {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSString * value = [userDefaults objectForKey:@"AppleInterfaceStyle"];
+    if ([value isEqualToString:@"Dark"]) {
+        self.darkModeOn = YES;
+    }
+    else {
+        self.darkModeOn = NO;
+    }
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (void)setImage{
+    
+    NSString * imageName = @"switch_off.png";
+    
+    if (_darkModeOn == YES) {
+        imageName = @"switch_on.png";
+    }
+    
+    _statusItem.image = [NSImage imageNamed:imageName];
+}
+
+- (void)toggled:(id)sender {
+    _darkModeOn = !_darkModeOn;
+    [self setImage];
+    
+    NSString * modeString = @"Light";
+    if (_darkModeOn) {
+        modeString = @"Dark";
+        CFPreferencesSetValue((CFStringRef)@"AppleInterfaceStyle", @"Dark", kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost);
+    }
+    else {
+        CFPreferencesSetValue((CFStringRef)@"AppleInterfaceStyle", NULL, kCFPreferencesAnyApplication, kCFPreferencesCurrentUser, kCFPreferencesCurrentHost);
+    }    
+}
+
+- (void)quitApp:(id)sender {
+    [self toggled:nil];
+    
+    NSUserNotification *notification = [[NSUserNotification alloc] init];
+    [notification setTitle: @"Flick quitting"];
+    [notification setSubtitle: @"Double clicking quits Flick"];
+    [notification setDeliveryDate:nil];
+    [[NSUserNotificationCenter defaultUserNotificationCenter] scheduleNotification: notification];
+    [NSApp terminate:self];
 }
 
 @end
